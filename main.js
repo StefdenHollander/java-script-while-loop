a7dc963900b4415163d872cd21ecf13445866c1b
S den Hollander <302045198@student.rocmondriaan.nl>
2023-09-29 12:47:41 +0200

    update

--- main.js
+++ main.js
@@ -1,10 +1,34 @@
 //opdracht 1
 
 let number = 0;
-while(number <10){
-console.log(number);
-number++;
+while (number < 10) {
+    console.log(number);
+    number++;
 }
+console.log('einde programma')
 
 //opdracht 2
 
+let number2 = 2;
+while (number2 < 21) {
+    console.log(number2);
+    number2++;
+}
+console.log('einde programma')
+//opdracht 3
+
+let number3 = 0;
+while (number3 < 10) {
+    console.log(number3);
+    number3++;
+}
+console.log('einde programma')
+
+    //opdracht 4
+
+    let number4 = 4;
+while (number4 < 40) {
+    console.log(number4);
+    number4++;
+}
+console.log('einde programma')
